f16c388e6a78d144cb6cd258f92363bbf710a5bf
alexdum123 <sasadumanskij515gmail,com>
2023-12-20 16:23:16 +0300

    create section-1

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function performSearch() {
-    var searchTerm = document.getElementById('search-input').value;
+// function performSearch() {
+//     var searchTerm = document.getElementById('search-input').value;
   
-    var searchResults = document.getElementById('search-results');
-    searchResults.innerHTML = '<p>Результаты поиска для: ' + searchTerm + '</p>';
+//     var searchResults = document.getElementById('search-results');
+//     searchResults.innerHTML = '<p>Результаты поиска для: ' + searchTerm + '</p>';
+// }
+  
+document.getElementById('search-input').addEventListener('keydown', function(event) {
+  // Проверяем, была ли нажата клавиша Enter (код клавиши 13)
+  if (event.keyCode === 27) {
+    // Выполняем поиск
+    performSearch();
   }
+});
+
+function performSearch() {
+  var searchTerm = document.getElementById('search-input').value;
+  var searchResults = document.getElementById('search-results');
+  searchResults.innerHTML = '<p>Результаты поиска для: ' + searchTerm + '</p>';
+}
