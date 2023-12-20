bd9c664aab6755014337db36677f9ea6063c3050
alexdum123 <sasadumanskij515gmail,com>
2023-12-20 17:02:11 +0300

    fix section-1

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// function performSearch() {
-//     var searchTerm = document.getElementById('search-input').value;
-  
-//     var searchResults = document.getElementById('search-results');
-//     searchResults.innerHTML = '<p>Результаты поиска для: ' + searchTerm + '</p>';
-// }
-  
 document.getElementById('search-input').addEventListener('keydown', function(event) {
-  // Проверяем, была ли нажата клавиша Enter (код клавиши 13)
   if (event.keyCode === 27) {
-    // Выполняем поиск
     performSearch();
   }
 });
